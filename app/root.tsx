82fda956e06906db3b69dfb87c3f99c150766019
FraserGrandfield <frasergrandfield110@gmail.com>
2025-05-05 04:02:36 +0100

    Added mui theme and call setup if dev

--- app/root.tsx
+++ app/root.tsx
@@ -6,10 +6,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
 import type { Route } from "./+types/root";
 import "./app.css";
 import Header from "./components/header/header";
+import { useEffect } from "react";
+import { setup } from "./setup";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,8 +44,23 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#dc1e83"
+    }
+  }
+});
+
 export default function App() {
-  return <Outlet />;
+  useEffect(() => {
+    import.meta.env.DEV && setup();
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <Outlet />
+    </ThemeProvider>
+  ) 
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
@@ -63,16 +80,18 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <Header>
-      <main className="pt-16 p-4 container mx-auto">
-        <h1>{message}</h1>
-        <p>{details}</p>
-        {stack && (
-          <pre className="w-full p-4 overflow-x-auto">
-            <code>{stack}</code>
-          </pre>
-        )}
-      </main>
-    </Header>
+    <ThemeProvider theme={theme}>
+      <Header>
+        <main className="pt-16 p-4 container mx-auto">
+          <h1>{message}</h1>
+          <p>{details}</p>
+          {stack && (
+            <pre className="w-full p-4 overflow-x-auto">
+              <code>{stack}</code>
+            </pre>
+          )}
+        </main>
+      </Header>
+    </ThemeProvider>
   );
 }
--- app/setup.ts
+++ app/setup.ts
@@ -1,65 +1,14 @@
-import { Fund } from "~/api";
+/**
+ * The file is only for testing.
+ * It will create a database 'cushon' in IndexDB and add mock funds.
+ */
+import { User } from "./api";
+import { mockFunds } from "./mockFunds";
 
-const mockFund1: Fund = {
-    id: "8779ddf9-101b-444d-8f72-972aab200741",
-    charges: 0.09,
-    label: "Cushon Equities Fund",
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et risus pretium, porta lacus sed, suscipit nisl. Aliquam ultricies sollicitudin erat sed dapibus. Aliquam at libero sit amet justo congue dapibus ac eu augue. Morbi at posuere velit. Nam ut felis eu eros ullamcorper volutpat sagittis ultricies odio. Phasellus quis tincidunt justo, a congue mauris. Suspendisse nec finibus elit. Etiam pretium velit ac sapien bibendum, sollicitudin pretium libero consequat. Donec et lorem et tortor pretium elementum. Cras in tincidunt felis. Aliquam a mauris ut nibh tristique semper. Pellentesque sagittis metus non quam varius, ac sodales massa viverra.",
-    riskLeveLevel: 2,
-    portfolioDescription: "Nullam sollicitudin purus elit, eget vestibulum ex congue cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec eget nisi risus. Nunc ut ultricies magna. Integer interdum nibh ex, at congue lorem tempor sed. Nunc sollicitudin, neque ac varius condimentum, ipsum velit ornare diam, eu finibus mauris purus non est. Suspendisse non maximus ipsum. Morbi imperdiet, tortor ut aliquam pulvinar, ligula augue tincidunt metus, eu rutrum justo tortor condimentum sapien. Suspendisse tincidunt, nibh ut ornare pretium, urna augue fringilla sapien, a pellentesque turpis velit in dolor. Sed ut convallis purus.",
-    portfolio: [
-        {
-            name: "Example 1",
-            percentage: 0.8,
-            about: "Cras in tincidunt felis. Aliquam a mauris ut nibh tristique semper. Pellentesque sagittis metus non quam varius, ac sodales massa viverra."
-        },
-        {
-            name: "Example 2",
-            percentage: 0.1,
-            about: "Praesent commodo massa in auctor hendrerit. Suspendisse nec consequat purus. Sed sit amet massa ac velit pellentesque euismod ut vel elit. Aliquam scelerisque, diam at dictum placerat, magna diam interdum est, sit amet suscipit eros odio eget neque."
-        },
-        {
-            name: "Example 3",
-            percentage: 0.1,
-            about: "Suspendisse non maximus ipsum. Morbi imperdiet, tortor ut aliquam pulvinar, ligula augue tincidunt metus, eu rutrum justo tortor condimentum sapien. Suspendisse tincidunt, nibh ut ornare pretium, urna augue fringilla sapien, a pellentesque turpis velit in dolor. Sed ut convallis purus."
-        }
-    ],
-    expectedPercentageIncrease: {
-        mostLikely: 0.004,
-        good: 0.0056,
-        bad: 0.0035
-    }
-}
-
-const mockFund2: Fund = {
-    id: "71a9ad34-8ceb-4949-a1b7-c28a16474fc8",
-    charges: 0.09,
-    label: "Cushon Ethical Fund",
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et risus pretium, porta lacus sed, suscipit nisl. Aliquam ultricies sollicitudin erat sed dapibus. Aliquam at libero sit amet justo congue dapibus ac eu augue. Morbi at posuere velit. Nam ut felis eu eros ullamcorper volutpat sagittis ultricies odio. Phasellus quis tincidunt justo, a congue mauris. Suspendisse nec finibus elit. Etiam pretium velit ac sapien bibendum, sollicitudin pretium libero consequat. Donec et lorem et tortor pretium elementum. Cras in tincidunt felis. Aliquam a mauris ut nibh tristique semper. Pellentesque sagittis metus non quam varius, ac sodales massa viverra.",
-    riskLeveLevel: 1,
-    portfolioDescription: "Nullam sollicitudin purus elit, eget vestibulum ex congue cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec eget nisi risus. Nunc ut ultricies magna. Integer interdum nibh ex, at congue lorem tempor sed. Nunc sollicitudin, neque ac varius condimentum, ipsum velit ornare diam, eu finibus mauris purus non est. Suspendisse non maximus ipsum. Morbi imperdiet, tortor ut aliquam pulvinar, ligula augue tincidunt metus, eu rutrum justo tortor condimentum sapien. Suspendisse tincidunt, nibh ut ornare pretium, urna augue fringilla sapien, a pellentesque turpis velit in dolor. Sed ut convallis purus.",
-    portfolio: [
-        {
-            name: "Example 1",
-            percentage: 0.3,
-            about: "Cras in tincidunt felis. Aliquam a mauris ut nibh tristique semper. Pellentesque sagittis metus non quam varius, ac sodales massa viverra."
-        },
-        {
-            name: "Example 2",
-            percentage: 0.5,
-            about: "Praesent commodo massa in auctor hendrerit. Suspendisse nec consequat purus. Sed sit amet massa ac velit pellentesque euismod ut vel elit. Aliquam scelerisque, diam at dictum placerat, magna diam interdum est, sit amet suscipit eros odio eget neque."
-        },
-        {
-            name: "Example 3",
-            percentage: 0.2,
-            about: "Suspendisse non maximus ipsum. Morbi imperdiet, tortor ut aliquam pulvinar, ligula augue tincidunt metus, eu rutrum justo tortor condimentum sapien. Suspendisse tincidunt, nibh ut ornare pretium, urna augue fringilla sapien, a pellentesque turpis velit in dolor. Sed ut convallis purus."
-        }
-    ],
-    expectedPercentageIncrease: {
-        mostLikely: 0.005,
-        good: 0.0065,
-        bad: 0.003
-    }
+const mockUser: User = {
+    id: "3910ce07-4462-4782-b388-670c8dd37164",
+    name: "Jhon Doe",
+    funds: []
 }
 
 export function setup() {
@@ -69,6 +18,9 @@ export function setup() {
             let db = request.result;
             if (!db.objectStoreNames.contains("funds")) {
                 db.createObjectStore("funds", { keyPath: 'id' });
+            }
+            if (!db.objectStoreNames.contains("users")) {
+                db.createObjectStore("users", { keyPath: 'id' });
             }
         };
         request.onsuccess = () => {
@@ -81,11 +33,13 @@ function addMockFund() {
     const request = indexedDB.open("cushon");
     request.onsuccess = () => {
         let db = request.result;
-        db.transaction('funds', 'readwrite')
-        .objectStore('funds')
-        .add(mockFund1);
-        db.transaction('funds', 'readwrite')
-        .objectStore('funds')
-        .add(mockFund2);
+        mockFunds.forEach(fund => {
+            db.transaction('funds', 'readwrite')
+            .objectStore('funds')
+            .add(fund);
+        });
+        db.transaction('users', 'readwrite')
+        .objectStore('users')
+        .add(mockUser);
     }
 }
