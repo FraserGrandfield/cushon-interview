77376cad206b3010ca14d0e25518095f9a4f15ed
FraserGrandfield <frasergrandfield110@gmail.com>
2025-05-05 13:26:36 +0100

    Added loading spinner

--- app/components/loading-spinner/loading-spinner.tsx
+++ app/components/loading-spinner/loading-spinner.tsx
@@ -1,5 +1,11 @@
 import { CircularProgress } from "@mui/material";
 
+/**
+ * Loading spinner component
+ * 
+ * @param { size } size of the spinner 
+ * @returns A React element that renders the loading spinner.
+ */
 export default function LoadingSpinner({ size }: {size: string}) {
     return (
         <CircularProgress id="loading-spinner" size={size} />
